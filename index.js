dfba612947d31094ee1f547beeab38c41e0f5736
Blairallen <arallen94@hotmail.com>
2024-04-06 15:49:37 -0700

    finctions lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqBlock = 42;
+    const distance = Math.abs(hqBlock - someValue);
+    return distance;
+  }
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feet = blocks * 264;
+    return feet;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const feetPerUnit = 264;
+    const distance = Math.abs(destination - start);
+    const distanceInFeet = distance * feetPerUnit;
+    return distanceInFeet;
+  }
+  function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(destination - start);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      const fare = (distance - 400) * 0.02;
+      return fare;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else if (distance > 2500) {
+      return 'cannot travel that far';
+    }
+  }
